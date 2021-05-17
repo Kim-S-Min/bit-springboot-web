09730accb391e13ff1c1e88455f11aa38d983005
김상민 <wrt123123123@gmail.com>
2021-05-17 16:44:50 +0900

    5.17 Commit

---
+++ src/main/java/com/example/academy/springboot/service/PostsService.java
@@ -0,0 +1,40 @@
+package com.example.academy.springboot.service;
+
+import com.example.academy.springboot.domain.post.Posts;
+import com.example.academy.springboot.domain.post.PostsRepository;
+import com.example.academy.springboot.web.dto.PostsResponseDto;
+import com.example.academy.springboot.web.dto.PostsSaveRequestDto;
+import com.example.academy.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    //  PostsApiController에서 update기능 수행
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    //  PostsApiController에서 findById기능 수행
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
